55e3b3496c59cecb6eedcf15fda9cc9352749b2f
urbanpotocnik <potocnik.urban123@gmail.com>
2024-12-09 14:48:21 +0100

    Cleaned the timing_utils.c code

--- System/Include/timing_utils.h
+++ System/Include/timing_utils.h
@@ -1,39 +1,10 @@
-
-/*
- * timing_utils.h
- *
- *  Created on:
- *      Author:
- */
-
-
 #ifndef INCLUDE_TIMING_UTILS_H_
 #define INCLUDE_TIMING_UTILS_H_
 
-
-
-// Pri implementaciji modula z orodji za merjenje časa bomo potrebovali sledeče HAL funkcije:
-//
-//	uint32_t HAL_GetTick (void )
-//
-
-
-
-
 // ----------- Include other modules (for public) -------------
-
-
-#include "stdint.h"		// za podporo definicijam celoštevilskih tipov (npr. uint8_t)
-
-
-
-
-
+#include "stdint.h"		
 
 // -------------------- Public definitions --------------------
-
-// Na tem mestu "javno" definiramo "handle" strukturo, s pomočjo katere
-// bomo lahko ustvarili funkcionalnost ure štoparice in tako merili čas.
 typedef struct
 {
 	uint32_t	time_mark;
@@ -41,28 +12,14 @@ typedef struct
 
 } stopwatch_handle_t;
 
-
-
-
-
-
 // ---------------- Public function prototypes ----------------
-
-
-// Funkcije za delo z uro štoparico.
 void TIMUT_stopwatch_set_time_mark(stopwatch_handle_t *stopwatch);
 void TIMUT_stopwatch_update(stopwatch_handle_t *stopwatch);
 uint8_t TIMUT_stopwatch_has_X_ms_passed(stopwatch_handle_t *stopwatch, uint32_t x);
 uint8_t TIMUT_stopwatch_has_another_X_ms_passed(stopwatch_handle_t *stopwatch, uint32_t x);
 
-
-// Demo testne funkcije.
 void TIMUT_stopwatch_demo(void);
-
 
 uint32_t TIMUT_get_stopwatch_elapsed_time(stopwatch_handle_t *stopwatch); //vaja 13
 
-
-
-
-#endif /* INCLUDE_TIMING_UTILS_H_ */
+#endif 
