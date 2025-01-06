7a824a13b0b34520eb7cac3eaf53503b25aefd55
urbanpotocnik <potocnik.urban123@gmail.com>
2025-01-06 22:17:25 +0100

    Optimized the game for speed, now it works fine

--- Application/game.c
+++ Application/game.c
@@ -157,20 +157,22 @@ void GamePlay_UpdateChanges(void) {
     }
 
 
-    if (TIMUT_stopwatch_has_another_X_ms_passed(&update_stopwatch_bird, settings.game_play_update_period)) {
+    if (TIMUT_stopwatch_has_another_X_ms_passed(&update_stopwatch_bird, 10)) {
         GFX_update_moving_gfx_object_location(&bird);
-        GFX_draw_one_gfx_object_on_background_via_DMA(&bird, &background);
+        GFX_draw_one_gfx_object_on_background(&bird, &background);
+        GFX_update_moving_gfx_object_location(&obstacleup);
+        GFX_draw_one_gfx_object_on_background(&obstacleup, &background);
+        GFX_update_moving_gfx_object_location(&obstacledown);
+        GFX_draw_one_gfx_object_on_background(&obstacledown, &background);
+
+
     }
 	
 
     if (TIMUT_stopwatch_has_another_X_ms_passed(&update_stopwatch_obstacle_up, settings.game_play_update_period + 10)) {
-        GFX_update_moving_gfx_object_location(&obstacleup);
-        GFX_draw_one_gfx_object_on_background_via_DMA(&obstacleup, &background);
     }
 
     if (TIMUT_stopwatch_has_another_X_ms_passed(&update_stopwatch_obstacle_down, settings.game_play_update_period + 20)) {
-        GFX_update_moving_gfx_object_location(&obstacledown);
-        GFX_draw_one_gfx_object_on_background_via_DMA(&obstacledown, &background);
     }
 }
 
@@ -207,24 +209,24 @@ uint8_t GamePlay() {
                     TIMUT_stopwatch_set_time_mark(&stopwatch_obstacle);
                 }
                 TIMUT_stopwatch_set_time_mark(&stopwatch_jump);
-                GFX_set_gfx_object_velocity(&bird, 0, 5);
+                GFX_set_gfx_object_velocity(&bird, 0, 1);
             }
 
             if (TIMUT_stopwatch_has_X_ms_passed(&stopwatch_jump, 500)) {
-                GFX_set_gfx_object_velocity(&bird, 0, -5);
+                GFX_set_gfx_object_velocity(&bird, 0, -1);
             }
 
             if (moving_obstacles == 1 && TIMUT_stopwatch_has_X_ms_passed(&stopwatch_obstacle, 4000)) {
                 // pozicija obstacle upa ni prava, je samo toliko da se sproba rendering 3 objektov
 				if (obstacle_spawned == 0) {
                     OBJ_init_obstacleup();
-                    GFX_set_gfx_object_velocity(&obstacleup, -2, 0);
+                    GFX_set_gfx_object_velocity(&obstacleup, -1, 0);
                     GFX_init_gfx_object_location(&obstacleup, 220, 220);
                     obstacle_spawned = 1;
                 }
                 if (obstacle_top_spawned == 0) {
                     OBJ_init_obstacledown();  
-                    GFX_set_gfx_object_velocity(&obstacledown, -2, 0);
+                    GFX_set_gfx_object_velocity(&obstacledown, -1, 0);
                     GFX_init_gfx_object_location(&obstacledown, 220, 0); 
                     obstacle_top_spawned = 1;
                 }
