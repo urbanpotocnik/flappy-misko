f8aa7e546fda99b82dcd03cc983c67bf28c2dc06
urbanpotocnik <potocnik.urban123@gmail.com>
2025-04-12 11:28:15 +0200

    Cleaned game.h

--- Firmware/Application/Include/game.h
+++ Firmware/Application/Include/game.h
@@ -1,15 +1,11 @@
 #ifndef INCLUDE_GAME_H_
 #define INCLUDE_GAME_H_
 
-// ----------- Include other modules (for public) -------------
-
-
 // -------------------- Public definitions --------------------
 #define INTRO_DELAY_BEFORE_KBD_ACTIVE 3000
 #define GAMEOVER_DELAY_BEFORE_KBD_ACTIVE 3000
 
 // ---------------- Public function prototypes ----------------
-
 void 		Game();
 uint8_t 	Intro();
 uint8_t 	GamePlay();
