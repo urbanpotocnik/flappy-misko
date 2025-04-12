6b24e077b72b4e1bcf25e5093cb2659bac5a1132
urbanpotocnik <potocnik.urban123@gmail.com>
2025-04-12 12:06:11 +0200

    Cleaned the repository

--- Firmware/Debug/System/buf.d
+++
@@ -1,3 +0,0 @@
-System/buf.o: ../System/buf.c \
- C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/buf.h
-C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/buf.h:
--- Firmware/Debug/System/ugui.d
+++
@@ -1,5 +0,0 @@
-System/ugui.o: ../System/ugui.c \
- C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/ugui.h \
- C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/ugui_config.h
-C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/ugui.h:
-C:/Users/Urban/Documents/Projects/flappy-misko/Firmware/System/Include/ugui_config.h:
